b3785d7549660d08d061f72512fd7a3326261891
tunahan-akargul <81328432+tunahan-akargul@users.noreply.github.com>
2025-12-27 22:09:38 +0300

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   build: {
+    outDir: 'docs',
     rollupOptions: {
       output: {
         manualChunks: {
